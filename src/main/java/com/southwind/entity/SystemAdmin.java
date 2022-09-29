f1ae9f9a866f5a1c3318acac3c6a0aa2b7415833
hyr <2591609426@qq.com>
2022-09-29 20:57:32 +0800

    第一次提交

---
+++ src/main/java/com/southwind/entity/SystemAdmin.java
@@ -0,0 +1,12 @@
+package com.southwind.entity;
+
+import lombok.Data;
+
+@Data
+public class SystemAdmin {
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+    private String telephone;
+}
